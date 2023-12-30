084b712fc5b96a9ba24aa5d2dc127795de074aa3
Vitalya <vitalyabes.vb@gmail.com>
2023-12-30 09:53:01 +0300

    Initial commit

---
+++ Naive_hash_function_for_a_string.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+int simple_string_hash(std::string str) {
+    int hash = 0;
+    for (int i = 0; i < str.size(); ++i) {
+        hash += static_cast<int> (str[i]);
+    }
+    return hash;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    std::string str;
+    do {
+        std::cout << "\nВведите строку: ";
+        std::cin >> str;
+        std::cout << "\nНаивный хэш строки " << str << " = " << simple_string_hash(str) << "\n";
+    } while (str != "exit");
+
+    return 0;
+
+}
